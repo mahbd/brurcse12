a653cf70d8e17252f14950389acafcef09b22582
mahbd <mahmudula2000@gmail.com>
2020-07-30 17:19:36 +0600

    make it better

--- judge_info/views.py
+++ judge_info/views.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 import requests
 from django.core.paginator import Paginator
 from django.http import HttpResponse
@@ -70,6 +72,8 @@ def cf_solves(request):
     if not response['correct']:
         return HttpResponse(response['status'])
     data = response['process']
+    print(data)
+    data.sort(key=itemgetter(1), reverse=True)
     context = {
         "title": "CF total solve",
         "all_person": data,

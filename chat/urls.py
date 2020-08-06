dc111b3c0eb19160579eca97cf7cdca88213e4e0
mahbd <mahmudula2000@gmail.com>
2020-08-06 08:55:25 +0600

    solved chat

--- chat/urls.py
+++ chat/urls.py
@@ -6,5 +6,6 @@ app_name = 'chat'
 urlpatterns = [
     path('', views.inbox, name='inbox'),
     path('send_message/', views.send_message, name='send'),
+    path('send_message/<int:recipient_id>', views.target_message, name='target_message'),
     path('<int:recipient_id>/', views.room, name='conversation'),
 ]
